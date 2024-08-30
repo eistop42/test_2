7b1f7d129eec979a2495e0ab11194a1a8b2fcab7
evdokimovilya <evdokimovilya@yandex.ru>
2024-08-30 17:18:51 +0500

    add

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hii, {name}')
+    asdf
     # Press Ctrl+F8 to toggle the breakpoint.adfasdfasdf
 
 
